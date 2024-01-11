a9586a5cf5f743a008d678bcded0e1300093ee16
Yoon Jun Pil <galaxy160102@gmail.com>
2024-01-11 13:39:01 +0900

    🛠️fix : User 도메인 삭제

--- src/main/java/com/jp/findhospital/domain/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jp.findhospital.domain.user.repository;
-
-import com.jp.findhospital.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
