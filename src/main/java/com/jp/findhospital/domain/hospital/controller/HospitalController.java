26b552f870b767fe3596e22a83f41acbfe18f646
Yoon Jun Pil <galaxy160102@gmail.com>
2024-02-23 20:49:25 +0900

    🛠️fix : ApiResponse 적용

--- src/main/java/com/jp/findhospital/domain/hospital/controller/HospitalController.java
+++ src/main/java/com/jp/findhospital/domain/hospital/controller/HospitalController.java
@@ -4,8 +4,10 @@ package com.jp.findhospital.domain.hospital.controller;
 import com.jp.findhospital.domain.comment.service.CommentService;
 import com.jp.findhospital.domain.hospital.entity.Hospital;
 import com.jp.findhospital.domain.hospital.service.HospitalService;
+import com.jp.findhospital.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,54 +23,54 @@ public class HospitalController {
 
     //전체 병원 조회
     @GetMapping("/hospitals")
-    public ResponseEntity<List<Hospital>> getHospitalAll(){
+    public ApiResponse<List<Hospital>> getHospitalAll(){
 
         List<Hospital> hospitalList = hospitalService.getHospitalAll();
 
-        return ResponseEntity.status(200).body(hospitalList);
+        return new ApiResponse<>(HttpStatus.OK,hospitalList);
     }
 
     //단일 병원 조회
     @GetMapping("/hospital/{id}")
-    public ResponseEntity<Hospital> getHospital(@PathVariable("id") Long id){
+    public ApiResponse<Hospital> getHospital(@PathVariable("id") Long id){
 
         Hospital hospital = hospitalService.getHospitalById(id);
 
-        return ResponseEntity.status(200).body(hospital);
+        return new ApiResponse<>(HttpStatus.OK,hospital);
     }
 
 
     //단일 병원 검색 (이름으로)
     @GetMapping("/search/name")
-    public ResponseEntity<List<Hospital>> getHospitalByName(@RequestParam String name){
+    public ApiResponse<List<Hospital>> getHospitalByName(@RequestParam String name){
         List<Hospital> hospitalList = hospitalService.getHospital(name);
 
-        return ResponseEntity.status(200).body(hospitalList);
+        return new ApiResponse<>(HttpStatus.OK,hospitalList);
     }
 
 
     //시도별 병원 검색
     @GetMapping("/search/province")
-    public ResponseEntity<List<Hospital>> getHospitalByProvince(@RequestParam String province){
+    public ApiResponse<List<Hospital>> getHospitalByProvince(@RequestParam String province){
         List<Hospital> hospitalList = hospitalService.getHospitalBySido(province);
 
-        return ResponseEntity.status(200).body(hospitalList);
+        return new ApiResponse<>(HttpStatus.OK,hospitalList);
     }
 
     //시군구별 병원 검색
     @GetMapping("/search/city")
-    public ResponseEntity<List<Hospital>> getHospitalByCity(@RequestParam String city){
+    public ApiResponse<List<Hospital>> getHospitalByCity(@RequestParam String city){
         List<Hospital> hospitalList = hospitalService.getHospitalBySiGunGu(city);
 
-        return ResponseEntity.status(200).body(hospitalList);
+        return new ApiResponse<>(HttpStatus.OK,hospitalList);
     }
 
     //병원타입별 병원 검색
     @GetMapping("/search/type")
-    public ResponseEntity<List<Hospital>> getHospitalByType(@RequestParam String type){
+    public ApiResponse<List<Hospital>> getHospitalByType(@RequestParam String type){
         List<Hospital> hospitalList = hospitalService.getHospitalByType(type);
 
-        return ResponseEntity.status(200).body(hospitalList);
+        return new ApiResponse<>(HttpStatus.OK,hospitalList);
     }
 
 
--- src/main/java/com/jp/findhospital/domain/hospital/service/HospitalService.java
+++ src/main/java/com/jp/findhospital/domain/hospital/service/HospitalService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.Optional;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true) //모든 메서드 단일 트랜잭션 내에서 수행, 읽기 전용 (수정 ㄴㄴ)
 public class HospitalService {
 
     private final HospitalRepository hospitalRepository;
